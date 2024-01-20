47f169178b4d0550b13cb5748dcedfa6a59aff72
Rudra Pratap Singh <99919139+Rudra-Rajput@users.noreply.github.com>
2024-01-20 08:12:47 +0000

    operations

--- 01-Basics/Conversion_operations.js
+++ 01-Basics/Conversion_operations.js
@@ -1,12 +1,12 @@
 let score = "25"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "25" => 25
 // "25abc" => NaN
@@ -15,13 +15,40 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 
 let valueInBoolean = Boolean(isLoggedIn)
-console.log(valueInBoolean);
-console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
 
 // 1 => true; 0 => false
 // "" => false; "hey" => true
 
 let age = 35
 let ageInString = String(age)
-console.log(ageInString);
-console.log(typeof ageInString);
+// console.log(ageInString);
+// console.log(typeof ageInString);
+
+
+// ****************** Operations *******************
+
+let value = 3
+
+let negValue = -value
+console.log(negValue);
+
+let Str1 = "hellow"
+let Str2 = " Rudra"
+
+let Str3 = Str1 + Str2
+console.log(Str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+let gameCounter = 100
+
+console.log(++gameCounter);
+console.log(gameCounter++);
+
+
+
