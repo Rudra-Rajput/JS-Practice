fe18d013543d62a994370efdf62f3f755a0dac77
Rudra Pratap Singh <99919139+Rudra-Rajput@users.noreply.github.com>
2024-01-27 13:40:01 +0000

    date_time

---
+++ 01-Basics/Date_Time.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toLocaleDateString());
+
+const currentDate = Date.now()
+// console.log(Math.round(currentDate/1000));
+
+const MyNewDate = new Date()
+// console.log(MyNewDate.getFullYear());
