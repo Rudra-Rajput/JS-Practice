3c9d0cd586ac3c1a80ec0d16676600fee7e278e7
Rudra Pratap Singh <99919139+Rudra-Rajput@users.noreply.github.com>
2024-01-27 12:14:36 +0000

    math_and_numbers

---
+++ 01-Basics/Number_and_Maths.js
@@ -0,0 +1,15 @@
+const score = 400;
+// console.log(score)
+
+const balance = new Number(100.636378)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const othersNumber = 10000000000;
+// console.log(othersNumber.toLocaleString('en-IN'));
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
